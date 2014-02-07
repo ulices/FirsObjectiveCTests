7dbaded7a0f09ad77002469694e013f808f8d03d
ulices <ulices.barajas@crowdint.com>
2014-02-07 10:46:36 -0600

    Initial Commit

---
+++ FirsObjectiveCTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirsObjectiveCTests
+//
+//  Created by Trainee 2 on 2/7/14.
+//  Copyright (c) 2014 Ulices. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "stsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([stsAppDelegate class]));
+    }
+}
---
+++ FirsObjectiveCTests/stsAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  stsAppDelegate.h
+//  FirsObjectiveCTests
+//
+//  Created by Trainee 2 on 2/7/14.
+//  Copyright (c) 2014 Ulices. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface stsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
